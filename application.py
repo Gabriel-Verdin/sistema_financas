eba96eaa941b610a547dbec988495bfbf066278a
Gabriel Verdin <gabrielg.verdin@gmail.com>
2026-06-30 10:59:23 -0300

    Implementa o módulo de formas de pagamento com opções de cadastro, listagem, edição e exclusão

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
-from functions import validacoes
-from functions import categorias
+from functions import validacoes, categorias, forma_pagamento
 
 def modulo_categoria():
     while True:
@@ -61,6 +60,64 @@ def modulo_categoria():
             else:
                 print('Digite uma opção existente!')
 
+def modulo_forma_pagamento():
+    while True:
+        print()
+        print('======== FORMAS DE PAGAMENTO ========')
+        print('[1] - Cadastrar Forma de Pagamento')
+        print('[2] - Listar Forma de Pagamento')
+        print('[3] - Editar Forma de Pagamento')
+        print('[4] - Apagar Forma de Pagamento')
+        print('[0] - Voltar')
+
+        print()
+        escolha_str = input('Escolha uma das opções: ')
+
+        escolha_int = validacoes.str_para_int(escolha_str)
+
+        if escolha_int is not None:
+            if escolha_int == 1:
+                nova_forma_pagamento = input('Digite a forma de pagamento que deseja adicionar: ')
+
+                forma_pagamento.cadastrar_forma_pagamento(nova_forma_pagamento)
+
+            elif escolha_int == 2:
+                print()
+                forma_pagamento.listar_forma_pagamento()
+
+            elif escolha_int == 3:
+                print()
+                forma_pagamento.listar_forma_pagamento()
+
+                print()
+                id_forma_pagamento_str = input('Escolha a forma de pagamento que deseja editar: ')
+
+                id_forma_pagamento_int = validacoes.str_para_int(id_forma_pagamento_str)
+
+                if id_forma_pagamento_int is not None:
+                    novo_nome = input('Digite o novo nome da forma de pagamento (nada para manter o atual): ')
+
+                    forma_pagamento.editar_forma_pagamento(id_forma_pagamento_int, novo_nome)
+
+            elif escolha_int == 4:
+                print()
+                forma_pagamento.listar_forma_pagamento()
+
+                print()
+                id_forma_pagamento_str = input('Escolha a forma de pagamento que deseja apagar: ')
+
+                id_forma_pagamento_int = validacoes.str_para_int(id_forma_pagamento_str)
+
+                if id_forma_pagamento_int is not None:
+                    forma_pagamento.apagar_forma_pagamento(id_forma_pagamento_int)
+
+            elif escolha_int == 0:
+                print('Voltando ao menu principal....')
+                break
+            
+            else:
+                print('Digite uma opção existente!')
+
 # ============================================== MAIN ==============================================  
 while True:
     print('==============================')
@@ -86,8 +143,7 @@ while True:
             modulo_categoria()
         
         elif escolha_int == 3:
-            print('Em andamento!')
-            print()
+            modulo_forma_pagamento()
 
         elif escolha_int == 0:
             print('Saindo....')
