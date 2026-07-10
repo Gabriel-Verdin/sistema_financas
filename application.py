92bfc5eb7700bc847384ad2a839c5e0a40fabdb9
Gabriel Verdin <gabrielg.verdin@gmail.com>
2026-07-10 11:47:54 -0300

    Adiciona validação para tipo de categoria ao cadastrar e editar categorias, garantindo entradas válidas.

--- application.py
+++ application.py
@@ -116,9 +116,11 @@ def modulo_categoria():
         if escolha_int is not None:
             if escolha_int == 1:
                 nova_categoria = input('Digite a categoria que deseja adicionar: ')
-                tipo_nova_categoria = input('Digite o tipo padrão da categoria (Despesa/Receita): ')
+                tipo_nova_categoria = input('Digite o tipo padrão da categoria (Despesa/Receita): ').lower()
+                tipo_nova_categoria_valida = validacoes.despesa_ou_receita(tipo_nova_categoria)
 
-                categorias.cadastrar_categoria(nova_categoria, tipo_nova_categoria)
+                if tipo_nova_categoria_valida is not None:
+                    categorias.cadastrar_categoria(nova_categoria, tipo_nova_categoria)
 
             elif escolha_int == 2:
                 print()
@@ -132,12 +134,16 @@ def modulo_categoria():
                 id_categoria_str = input('Escolha a categoria que deseja editar: ')
 
                 id_categoria_int = validacoes.str_para_int(id_categoria_str)
+                id_categoria_valida = validacoes.escolha_valida_categoria(id_categoria_int)
 
-                if id_categoria_int is not None:
+                if id_categoria_int is not None and id_categoria_valida is not None:
                     novo_nome = input('Digite o novo nome da categoria (nada para manter o atual): ')
-                    novo_tipo = input('Digite o novo tipo da categoria (nada para manter o atual): ')
+                    novo_tipo = input('Digite o novo tipo da categoria (nada para manter o atual): ').lower()
 
-                    categorias.editar_categoria(id_categoria_int, novo_nome, novo_tipo)
+                    novo_tipo_valido = validacoes.despesa_ou_receita(novo_tipo)
+
+                    if novo_tipo_valido is not None:
+                        categorias.editar_categoria(id_categoria_int, novo_nome, novo_tipo)
 
             elif escolha_int == 4:
                 print()
@@ -147,8 +153,9 @@ def modulo_categoria():
                 id_categoria_str = input('Escolha a categoria que deseja apagar: ')
 
                 id_categoria_int = validacoes.str_para_int(id_categoria_str)
+                id_categoria_valida = validacoes.escolha_valida_categoria(id_categoria_int)
 
-                if id_categoria_int is not None:
+                if id_categoria_int is not None and id_categoria_valida is not None:
                     categorias.apagar_categoria(id_categoria_int)
 
             elif escolha_int == 0:
