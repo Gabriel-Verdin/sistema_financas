ce04a0a1ef5787078d52de4c64b868ad011fd35c
Gabriel Verdin <gabrielg.verdin@gmail.com>
2026-07-16 08:10:01 -0300

    Adiciona botões para cadastrar, mostrar, editar e apagar categorias na interface gráfica

--- tkinter_project/modules/modulo_categoria.py
+++ tkinter_project/modules/modulo_categoria.py
@@ -4,5 +4,17 @@ def tela_modulo_categoria(tela, mostrar_tela, tela_menu_principal):
     label_modulo_categoria = tk.Label(tela, text='===== Módulo Categoria =====')
     label_modulo_categoria.pack(pady=20)
 
+    botao_cadastrar_categoria = tk.Button(tela, text='Cadastrar Categoria', command=lambda: None)
+    botao_cadastrar_categoria.pack(pady=20)
+
+    botao_mostrar_categoria = tk.Button(tela, text='Mostrar Categorias', command=lambda: None)
+    botao_mostrar_categoria.pack(pady=20)
+
+    botao_editar_categoria = tk.Button(tela, text='Editar Categoria', command=lambda: None)
+    botao_editar_categoria.pack(pady=20)
+
+    botao_apagar_categoria = tk.Button(tela, text='Apagar Categoria', command=lambda: None)
+    botao_apagar_categoria.pack(pady=20)
+
     botao_voltar_menu_principal = tk.Button(tela, text='Voltar ao menu', command=lambda: mostrar_tela(tela_menu_principal))
     botao_voltar_menu_principal.pack(pady=20)
