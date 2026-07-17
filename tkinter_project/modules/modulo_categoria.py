6b8f49a7e889b86711b00d2e5b8cee8384393b91
Gabriel Verdin <gabrielg.verdin@gmail.com>
2026-07-17 10:08:22 -0300

    Adiciona funcionalidade de voltar ao módulo na tela de categorias e ajusta a chamada da função para mostrar categorias

--- tkinter_project/modules/modulo_categoria.py
+++ tkinter_project/modules/modulo_categoria.py
@@ -1,9 +1,19 @@
 import tkinter as tk
 
-def tela_mostrar_categoria(tela_categoria, mostrar_tela):
+def tela_mostrar_categoria(tela_categoria, mostrar_tela, tela_modulo_categoria):
+    # Levanta a tela
     mostrar_tela(tela_categoria)
+
+    # Limpa todos os widgets existentes
+    for widget in tela_categoria.winfo_children():
+        widget.destroy()
+    
+    # Cria os widgets novamente
     label_cadastro = tk.Label(tela_categoria, text="Categorias")
     label_cadastro.pack(pady=20)
+
+    botao_voltar = tk.Button(tela_categoria, text='Voltar ao Módulo', command=lambda: mostrar_tela(tela_modulo_categoria))
+    botao_voltar.pack(pady=20)
 
 def tela_modulo_categoria(tela, mostrar_tela, tela_menu_principal, frame_mostrar_categorias):
     label_modulo_categoria = tk.Label(tela, text='===== Módulo Categoria =====')
@@ -12,7 +22,7 @@ def tela_modulo_categoria(tela, mostrar_tela, tela_menu_principal, frame_mostrar
     botao_cadastrar_categoria = tk.Button(tela, text='Cadastrar Categoria', command=lambda: None)
     botao_cadastrar_categoria.pack(pady=20)
 
-    botao_mostrar_categoria = tk.Button(tela, text='Mostrar Categorias', command=lambda: tela_mostrar_categoria(frame_mostrar_categorias, mostrar_tela))
+    botao_mostrar_categoria = tk.Button(tela, text='Mostrar Categorias', command=lambda: tela_mostrar_categoria(frame_mostrar_categorias, mostrar_tela, tela))
     botao_mostrar_categoria.pack(pady=20)
 
     botao_editar_categoria = tk.Button(tela, text='Editar Categoria', command=lambda: None)
